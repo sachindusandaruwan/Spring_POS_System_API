2615643e54d7c69edc4f33b5c9de6b30044e0460
sachindusandaruwan <sadaruwansachindu@gmail.com>
2024-10-09 15:38:20 +0530

    add log add test customer save

--- src/main/java/lk/ijse/gdse68/aad/spring_pos_system_api/controller/CustomerController.java
+++ src/main/java/lk/ijse/gdse68/aad/spring_pos_system_api/controller/CustomerController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("api/v1/customer")
@@ -16,16 +18,20 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class CustomerController {
 
+    Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
     @Autowired
     private final CustomerService customerService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveCustomer(@RequestBody CustomerDto customerDto ) {
       if (customerDto==null){
+          logger.error("CustomerDto is null");
           return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
       } else {
           try {
               customerService.saveCustomer(customerDto);
+              logger.info("Customer saved successfully");
               return new ResponseEntity<>(HttpStatus.CREATED);
           }catch (DataPersistFailException e){
               return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
